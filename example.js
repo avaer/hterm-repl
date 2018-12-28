5a7b1bc89396c57aab24f41cebd06e43e04abea2
Avaer Kazmer <a@modules.io>
2018-12-27 19:11:36 -0500

    Update example.js

--- example.js
+++ example.js
@@ -1,20 +1,25 @@
 const htermRepl = require('.');
 
-const replServer = htermRepl({
+htermRepl({
   port: 8000,
-});
-replServer.on('repl', r => {
-  r.setEval((s, context, filename, cb) => {
-    let err = null, result;
-    try {
-      result = eval(s);
-    } catch (e) {
-      err = e;
-    }
-    if (!err) {
-      cb(null, result);
-    } else {
-      cb(err);
-    }
-  });
+}, (err, replServer) => {
+  if (!err) {
+    replServer.on('repl', r => {
+      r.setEval((s, context, filename, cb) => {
+        let err = null, result;
+        try {
+          result = eval(s);
+        } catch (e) {
+          err = e;
+        }
+        if (!err) {
+          cb(null, result);
+        } else {
+          cb(err);
+        }
+      });
+    });
+  } else {
+    throw err;
+  }
 });
