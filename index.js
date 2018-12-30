6fd53615065c21f756b164e75d27d5b3810a3079
Avaer Kazmer <a@modules.io>
2018-12-29 19:19:57 -0500

    Major protocol refactor in index.js

--- index.js
+++ index.js
@@ -3,40 +3,15 @@ const {EventEmitter} = events;
 const stream = require('stream');
 const path = require('path');
 const fs = require('fs');
+const url = require('url');
 const http = require('http');
 const repl = require('repl');
 const ws = require('ws');
 
-const DEFAULT_PORT = 9223;
+// const DEFAULT_PORT = 9223;
 
-module.exports = ({port = DEFAULT_PORT}, cb) => {
-  const app = (req, res) => {
-    if (req.method === 'GET') {
-      const {p, type} = (() => {
-        if (req.url === '/js/hterm_all.js') {
-          return {
-            p: path.join(__dirname, 'js', 'hterm_all.js'),
-            type: 'application/javascript',
-          };
-        } else {
-          return {
-            p: path.join(__dirname, 'index.html'),
-            type: 'text/html; charset=utf-8',
-          };
-        }
-      })();
-      res.setHeader('Content-Type', type);
-      fs.createReadStream(p)
-        .pipe(res);
-    } else {
-      res.end();
-    }
-  };
-
-  const wss = new ws.Server({
-    noServer: true
-  });
-  const _makeRepl = (ws, spec) => {
+module.exports = ({port = null}, cb) => {
+  const _makeRepl = (socket, id) => {
     let live = true;
 
     const input = new stream.Readable();
@@ -46,7 +21,7 @@ module.exports = ({port = DEFAULT_PORT}, cb) => {
       if (encoding !== 'buffer') {
         chunk = Buffer.from(chunk, encoding);
       }
-      ws.send(chunk);
+      socket.send(chunk);
 
       next();
     };
@@ -71,7 +46,7 @@ module.exports = ({port = DEFAULT_PORT}, cb) => {
     };
     let r = _makeInstance();
     _bindInstance(r);
-    ws.on('message', m => {
+    socket.on('message', m => {
       const j = JSON.parse(m);
       const {method, args} = j;
       switch (method) {
@@ -88,34 +63,128 @@ module.exports = ({port = DEFAULT_PORT}, cb) => {
         }
       }
     });
-    ws.on('close', () => {
+    socket.on('close', () => {
       live = false;
       r.close();
     });
 
     let localEval = () => {};
     return {
+      id,
       setEval(newEval) {
         localEval = newEval;
       },
       close() {
-        ws.close();
+        socket.close();
       },
     };
   };
-
   const replServer = new EventEmitter();
+  replServer.createConnection = u => {
+    const {query} = url.parse(u, true);
+    const {id} = query;
 
-  const server = http.createServer(app);
-  server.on('upgrade', (request, socket, head) => {
-    wss.handleUpgrade(request, socket, head, ws => {
-      const r = _makeRepl(ws);
-      r.url = request.url;
-      replServer.emit('repl', r);
+    const socket = new stream.Transform();
+    const bs = [];
+    let bsBytes = 0;
+    socket._transform = (chunk, encoding, cb) => { // input from page
+      bs.push(chunk);
+      bsBytes += chunk.byteLength;
+
+      for (;;) {
+        if (bsBytes >= Uint32Array.BYTES_PER_ELEMENT) {
+          while (bs[0].byteLength < Uint32Array.BYTES_PER_ELEMENT) {
+            const bsToConcat = [];
+            let bsToConcatBytes = 0;
+            while (bsToConcatBytes < Uint32Array.BYTES_PER_ELEMENT) {
+              const b = bs.pop();
+              bsToConcat.push(b);
+              bsToConcatBytes += b.byteLength;
+            }
+            bs.unshift(Buffer.concat(bsToConcat));
+          }
+        
+          const messageByteLength = new Uint32Array(bs[0].buffer, bs[0].byteOffset, 1)[0];
+
+          if (bsBytes >= Uint32Array.BYTES_PER_ELEMENT + messageByteLength) {
+            const b = bs.length === 1 ? bs[0] : Buffer.concat(bs);
+            bs.length = 0;
+
+            const bMessage = b.slice(Uint32Array.BYTES_PER_ELEMENT, Uint32Array.BYTES_PER_ELEMENT + messageByteLength);
+            const bTail = b.slice(Uint32Array.BYTES_PER_ELEMENT + messageByteLength);
+
+            const m = bMessage.toString('utf8');
+            socket.emit('message', m);
+
+            bsBytes -= (Uint32Array.BYTES_PER_ELEMENT + messageByteLength);
+            if (bTail.byteLength > 0) {
+              bs.push(bTail);
+            }
+          } else {
+            break;
+          }
+        } else {
+          break;
+        }
+      }
+      
+      cb();
+    };
+    socket.send = chunk => { // output to page
+      socket.push(chunk);
+    };
+
+    process.nextTick(() => {
+      const r = _makeRepl(ws, id);
+      replServer.emit('repl', socket);
     });
-  });
-  server.on('error', cb);
-  server.listen(port, () => {
-    cb(null, replServer);
-  });
+
+    return socket;
+  };
+
+  if (port) {
+    const app = (req, res) => {
+      if (req.method === 'GET') {
+        const {p, type} = (() => {
+          if (req.url === '/js/hterm_all.js') {
+            return {
+              p: path.join(__dirname, 'js', 'hterm_all.js'),
+              type: 'application/javascript',
+            };
+          } else {
+            return {
+              p: path.join(__dirname, 'index.html'),
+              type: 'text/html; charset=utf-8',
+            };
+          }
+        })();
+        res.setHeader('Content-Type', type);
+        fs.createReadStream(p)
+          .pipe(res);
+      } else {
+        res.end();
+      }
+    };
+    const server = http.createServer(app);
+    const wss = new ws.Server({
+      noServer: true
+    });
+    server.on('upgrade', (request, socket, head) => {
+      wss.handleUpgrade(request, socket, head, ws => {
+        const {query} = url.parse(req.url, true);
+        const {id} = query;
+
+        const r = _makeRepl(ws, id);
+        replServer.emit('repl', r);
+      });
+    });
+    server.on('error', cb);
+    server.listen(port, () => {
+      cb(null, replServer);
+    });
+  } else {
+    process.nextTick(() => {
+      cb(null, replServer);
+    });
+  }
 };
